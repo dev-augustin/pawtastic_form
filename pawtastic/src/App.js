95fec4a667d5785a226ec0a456802a3372e9a494
dev-augustin <dev.augustin00@gmail.com>
2020-03-29 22:30:01 -0500

    updated App function to Class component

--- pawtastic/src/App.js
+++ pawtastic/src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>Lorem10</h1>
-      </header>
-     <form>
-       <label>
-        Name
-        <br/>
-        <input/>
-       </label>
-     </form>
-    </div>
-  );
+class App extends React.Component() {
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <h1>Lorem10</h1>
+        </header>
+       <form>
+         <label>
+          Name
+          <br/>
+          <input/>
+         </label>
+       </form>
+      </div>
+    );
+    
+  }
 }
 
 export default App;
