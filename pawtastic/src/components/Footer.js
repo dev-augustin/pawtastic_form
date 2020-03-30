d0a181371d4845c9257b3fe1ae8af114bb1dc4d6
dev-augustin <dev.augustin00@gmail.com>
2020-03-29 23:40:02 -0500

    added footer file

---
+++ pawtastic/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <div className="title">
+            <h1>Yay, we love dogs! Give us the basics about your pup.</h1>
+        </div>
+    );
+
+}
+
+export default Footer;
