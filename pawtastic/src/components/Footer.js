9200215e6dbea6375813864ed326fbe201fdb7a8
dev-augustin <dev.augustin00@gmail.com>
2020-03-30 01:03:41 -0500

    organised folder structures

--- pawtastic/src/components/Footer.js
+++ pawtastic/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(){
     return(
         <div className="title">
-            <h1>Yay, we love dogs! Give us the basics about your pup.</h1>
+            <h3>Yay, we love dogs! Give us the basics about your pup.</h3>
         </div>
     );
 
