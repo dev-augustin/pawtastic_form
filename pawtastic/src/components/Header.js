31236f4f3223d7de77fca6a5e42f0278983accbf
dev-augustin <dev.augustin00@gmail.com>
2020-03-29 23:14:51 -0500

    added components folder

---
+++ pawtastic/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <div className="title">
+            <h1>Yay, we love dogs! Give us the basics about your pup.</h1>
+        </div>
+    );
+
+}
+
+export default Header;
---
+++ pawtastic/src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav(){
+    return(
+        <div className="title">
+            <h1>Yay, we love dogs! Give us the basics about your pup.</h1>
+        </div>
+    );
+
+}
+export default Nav;
