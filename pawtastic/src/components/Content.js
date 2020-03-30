7c254931f2c98f2d86cc01b938f98dfac11e8059
dev-augustin <dev.augustin00@gmail.com>
2020-03-30 18:02:28 -0500

    added toggle button

--- pawtastic/src/components/Content.js
+++ pawtastic/src/components/Content.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
-function Content(){
-    return(
-        <div className="title">
-            <h3>Yay, we love dogs! Give us the basics about your pup.</h3>
-        </div>
-    );
+class Content extends React.Component{
+//    constructor(props){
+//        super(props);
+//        this.state={value: ''};
+
+//        this.handleChange = this.handleChange.bind(this);
+//        this.handleSubmit=this.handleSubmit.bind(this);
+//    }
+
+    render(){  
+        return(
+            <React.Fragment>
+                 <div className="title">
+                
+              Name<input type='text' id='name'></input>
+              <img src="" alt="camera"/>Upload Photo<br/>
+              <input type='checkbox' id="toggle" className= 'checkbox'/>
+              <label for='toggle' class='switch'></label>
+            
+            
+            
+            </div>
+            </React.Fragment>
+           
+        );
+    }
 
 }
 
